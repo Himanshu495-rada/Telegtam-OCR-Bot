79d22c4500986f8497ffaf3816a31cc6ae058245
Himanshu Tekade <61615912+Himanshu495-rada@users.noreply.github.com>
2021-11-07 18:16:36 +0530

    Update ocr_python.py

--- ocr_python.py
+++ ocr_python.py
@@ -1,20 +1,10 @@
-# from PIL import Image
-# import pytesseract
-# import numpy as np
-
-# filename = 'C:\\Users\\himan\\Desktop\\my_programs\\python\\Telegram Bot\\file_5.jpg'
-# img1 = np.array(Image.open(filename))
-# text = pytesseract.image_to_string(img1)
-
-# print(text)
-
 import numpy as np
 from PIL import Image
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 
-file = "file_5.jpg"
+file = "file.jpg"
 
 img = np.array(Image.open(file))
 
